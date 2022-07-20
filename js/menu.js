2df7dfbbf9b4bf73a3953f8b1b1737e02ae2a30f
Анисимов Александр <ujc90@mail.ru>
2022-07-20 08:35:54 +0300

    добавлена форма обратной связи

--- js/menu.js
+++ js/menu.js
@@ -12,15 +12,20 @@ burger.addEventListener('click', function () {
 let btn = document.querySelector('.knopka')
 let tel = document.querySelector('.phone-button')
 let wats = document.querySelector('.whatsapp-button')
+let inline = document.querySelector('#inline')
+let letter = document.querySelector('.absolute_message')
+let close = document.querySelector('#close')
 //
 function magic() {
   if (window.pageYOffset > 20) {
     btn.style.opacity = '1';
     tel.style.opacity = '1';
     wats.style.opacity = '1';
+    letter.style.opacity = '1';
   } else { btn.style.opacity = '0';
   tel.style.opacity = '0';
   wats.style.opacity = '0';
+  letter.style.opacity = '0';
 }
 }
 //
@@ -28,7 +33,14 @@ btn.onclick = function () {
 	window.scrollTo(0,0)
 }
 
+letter.addEventListener('click', function () {
+  inline.style.display = "block";
+})
+close.addEventListener('click', function () {
+  inline.style.display = "none";
+})
 // When scrolling, we run the function
 window.onscroll = magic
+
 
 }
