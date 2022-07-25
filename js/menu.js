59d8559b9316e0ba64c24262052d9cffdc5adbeb
Анисимов Александр <ujc90@mail.ru>
2022-07-26 01:26:47 +0300

    Сайт требует доработки формы обратной связи

--- js/menu.js
+++ js/menu.js
@@ -1,46 +1,48 @@
-window.onload = function() {
+window.onload = function () {
 
-let burger = document.querySelector('#nav-icon6');
-burger.addEventListener('click', function () {
-  burger.classList.toggle('open');
-  let menuMobile = document.querySelector('.wrapper_menu');
-  let logo = document.querySelector('.logo')
-  menuMobile.classList.toggle('wrapper_menu_open');
-  logo.classList.toggle('logo_swap');
-})
+  let burger = document.querySelector('#nav-icon6');
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('open');
+    let menuMobile = document.querySelector('.wrapper_menu');
+    let logo = document.querySelector('.logo')
+    menuMobile.classList.toggle('wrapper_menu_open');
+    document.querySelector('body').classList.toggle('body_overflow');
+    logo.classList.toggle('logo_swap');
+  })
 
-let btn = document.querySelector('.knopka')
-let tel = document.querySelector('.phone-button')
-let wats = document.querySelector('.whatsapp-button')
-let inline = document.querySelector('#inline')
-let letter = document.querySelector('.absolute_message')
-let close = document.querySelector('#close')
-//
-function magic() {
-  if (window.pageYOffset > 20) {
-    btn.style.opacity = '1';
-    tel.style.opacity = '1';
-    wats.style.opacity = '1';
-    letter.style.opacity = '1';
-  } else { btn.style.opacity = '0';
-  tel.style.opacity = '0';
-  wats.style.opacity = '0';
-  letter.style.opacity = '0';
-}
-}
-//
-btn.onclick = function () {
-	window.scrollTo(0,0)
-}
+  let btn = document.querySelector('.knopka')
+  let tel = document.querySelector('.phone-button')
+  let wats = document.querySelector('.whatsapp-button')
+  let inline = document.querySelector('#inline')
+  let letter = document.querySelector('.absolute_message')
+  let close = document.querySelector('#close')
+  //
+  function magic() {
+    if (window.pageYOffset > 20) {
+      btn.style.opacity = '1';
+      tel.style.opacity = '1';
+      wats.style.opacity = '1';
+      letter.style.opacity = '1';
+    } else {
+      btn.style.opacity = '0';
+      tel.style.opacity = '0';
+      wats.style.opacity = '0';
+      letter.style.opacity = '0';
+    }
+  }
+  //
+  btn.onclick = function () {
+    window.scrollTo(0, 0)
+  }
 
-letter.addEventListener('click', function () {
-  inline.style.display = "block";
-})
-close.addEventListener('click', function () {
-  inline.style.display = "none";
-})
-// When scrolling, we run the function
-window.onscroll = magic
+  letter.addEventListener('click', function () {
+    inline.style.display = "block";
+  })
+  close.addEventListener('click', function () {
+    inline.style.display = "none";
+  })
+  // When scrolling, we run the function
+  window.onscroll = magic
 
 
 }
--- js/slider.js
+++ js/slider.js
@@ -1,75 +1,75 @@
-window.onload = function() {
-    
- 
-
-let slideIndex = 1;
-let slides = document.getElementsByClassName("myslide");
-console.log(slides)
-showSlides(slideIndex);
-
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
-
-next.addEventListener("click", function() {
-  showSlides(slideIndex += 1);
-  clearInterval(timer);
-  timer = setInterval(function() {
-    slideIndex++;
-    showSlides(slideIndex);
-  }, 3000);
-});
-
-prev.addEventListener("click", function() {
-  showSlides(slideIndex -= 1);
-  clearInterval(timer);
-  timer = setInterval(function() {
-    slideIndex++;
-    showSlides(slideIndex);
-  }, 3000);
-
-});
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+window.onload = function () {
 
 
-function showSlides(n) {
-   slides = document.getElementsByClassName("myslide");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
 
-  for (let slide of slides) {
-    slide.classList.remove('myslide__active');
-    //slide.style.display = "none";
-  }
-  slides[slideIndex - 1].classList.add('myslide__active');
-  //slides[slideIndex - 1].style.display = "flex"; 
-}
-
-let timer = setInterval(function() {
-  slideIndex++;
+  let slideIndex = 1;
+  let slides = document.getElementsByClassName("myslide");
+  console.log(slides)
   showSlides(slideIndex);
-}, 5000);
 
-/// Бургер меню
+  let prev = document.getElementById('prev');
+  let next = document.getElementById('next');
 
-let burger = document.querySelector('#nav-icon6');
-burger.addEventListener('click', function () {
-  burger.classList.toggle('open');
-  let menuMobile = document.querySelector('.wrapper_menu');
-  let logo = document.querySelector('.logo')
-  menuMobile.classList.toggle('wrapper_menu_open');
-  logo.classList.toggle('logo_swap');
-  
-})
+  next.addEventListener("click", function () {
+    showSlides(slideIndex += 1);
+    clearInterval(timer);
+    timer = setInterval(function () {
+      slideIndex++;
+      showSlides(slideIndex);
+    }, 3000);
+  });
+
+  prev.addEventListener("click", function () {
+    showSlides(slideIndex -= 1);
+    clearInterval(timer);
+    timer = setInterval(function () {
+      slideIndex++;
+      showSlides(slideIndex);
+    }, 3000);
+
+  });
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
 
 
- 
+  function showSlides(n) {
+    slides = document.getElementsByClassName("myslide");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+
+    for (let slide of slides) {
+      slide.classList.remove('myslide__active');
+      //slide.style.display = "none";
+    }
+    slides[slideIndex - 1].classList.add('myslide__active');
+    //slides[slideIndex - 1].style.display = "flex"; 
+  }
+
+  let timer = setInterval(function () {
+    slideIndex++;
+    showSlides(slideIndex);
+  }, 5000);
+
+  /// Бургер меню
+
+  let burger = document.querySelector('#nav-icon6');
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('open');
+    let menuMobile = document.querySelector('.wrapper_menu');
+    let logo = document.querySelector('.logo')
+    menuMobile.classList.toggle('wrapper_menu_open');
+    logo.classList.toggle('logo_swap');
+
+  })
+
+
+
 
 };
 
